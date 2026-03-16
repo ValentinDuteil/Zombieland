42a70eec66ce83d22e9f8f6201cbd9868e92f9c2
macepeter <peter.mace@oclock.school>
2026-03-16 12:06:43 +0100

    feat: road-and-controller-getactivities

--- server/src/controllers/activities.ts
+++ server/src/controllers/activities.ts
@@ -1,1 +1,14 @@
 // Business logic for attractions : handles requests and responses
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const getActivities = async (req, res) => {
+    try {
+        const activities = await prisma.attraction.findMany()
+        res.json(activities)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Erreur lors de la récupération des activités" })
+    }
+}
