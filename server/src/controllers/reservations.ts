65a6f15e3b1eb549af1ff13ad4a3efd5a25d8224
DaphnéAutrive <daphne.dematosp@icloud.com>
2026-03-16 11:46:39 +0100

    feat: add get reservations controller

--- server/src/controllers/reservations.ts
+++ server/src/controllers/reservations.ts
@@ -1,1 +1,18 @@
 // Business logic for reservations : handles requests and responses
+
+import type { Request, Response } from "express";
+// Talk to the db
+import { prisma } from '../lib/prisma.js'
+
+// Retrieves all reservations for the logged-in member
+export const getReservations = async (req: Request, res: Response) => {
+    try {
+        // Query the db to retrieve all reservations
+        const reservations = await prisma.reservation.findMany()
+        // Return reservations whit a 200 status (success)
+        res.status(200).json(reservations)
+    } catch {
+        // In cas of server error, return a 500 status
+        res.status(500).json({ message: 'Internal server error' })
+    }
+}
