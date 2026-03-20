5217c8dc15168fb80d07d258bfda017cc85cb31e
yohann422 <fufulelapinou@gmail.com>
2026-03-20 09:52:52 +0100

    fix: secure update

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -63,7 +63,7 @@ export async function register(req: Request, res: Response, next: NextFunction) 
   //8.putting the token in a cookie httpOnly
   res.cookie('token', token, {
     httpOnly: true,                 // JavaScript can't read it → protection XSS
-    secure: false,                  // false for dev (HTTP, localhost), true for prod (HTTPS uniquement)
+    secure: true,                  // false for dev (HTTP, localhost), true for prod (HTTPS uniquement)
     sameSite: 'lax',                // cookie is send only from the same website → CSRF protection
     maxAge: 7 * 24 * 60 * 60 * 1000 // durée de vie en millisecondes
   })
@@ -118,7 +118,7 @@ export async function login(req: Request, res: Response, next: NextFunction) {
   //7.putting the token in a cookie httpOnly
   res.cookie('token', token, {
     httpOnly: true,                 // JavaScript can't read it → protection XSS
-    secure: false,                  // false for dev (HTTP, localhost), true for prod (HTTPS uniquement)
+    secure: true,                  // false for dev (HTTP, localhost), true for prod (HTTPS uniquement)
     sameSite: 'lax',                // cookie is send only from the same website → CSRF protection
     maxAge: 7 * 24 * 60 * 60 * 1000 // durée de vie en millisecondes
   })
