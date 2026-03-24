e5c778eb2a036d518b255027c4fc3aadeac5f2a6
yohann422 <fufulelapinou@gmail.com>
2026-03-24 11:54:27 +0100

    test: all local tests passed for admin create reservation

--- server/src/routes/reservations.routes.ts
+++ server/src/routes/reservations.routes.ts
@@ -17,6 +17,7 @@ const router = Router()
 router.get('/me', checkToken, getMyReservations)
 // When someone calls GET /api/reservations execute getAllReservations (admin only)
 router.get('/', checkToken, checkRole("ADMIN"), getAllReservations)
+// When someone calls POST /api/admin execute createReservation
 router.post('/admin', checkToken, checkRole('ADMIN'), validate(AdminCreateReservationSchema), createReservation)
 // When someone calls POST /api/reservations, execute createReservation
 router.post('/', checkToken, validate(createReservationSchema), createReservation)
