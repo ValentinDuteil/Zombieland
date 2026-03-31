4c54b6d43110504e847653e8019532682e49e1d8
DaphnéAutrive <daphne.dematosp@icloud.com>
2026-03-31 14:12:09 +0200

    feat: color button

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -127,7 +127,7 @@ function Login() {
             onClick={handleSubmit}
             bgImage={`url(${bgBouton})`}
             color="zombieland.secondary"
-            _hover={{ bg: "zombieland.cta2orange" }}
+            _hover={{ bg: "zombieland.cta2orange", color: "zombieland.white" }}
             fontFamily="body"
             fontSize="20px"
             py={6}
